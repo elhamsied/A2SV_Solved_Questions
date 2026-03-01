d838330310f53e8aadd7676a8f62edbdca81eaf2
Elham seid <elhamsied87@gmail.com>
2026-03-01 20:30:32 +0300

    Implement diagonal traversal of a matrix

---
+++ diagonal-matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        m, n = len(mat), len(mat[0])
+        result = []
+
+        row = col = 0
+        direction = 1
+        for _ in range(m * n):
+            result.append(mat[row][col])
+
+            if direction == 1:
+                if col == n - 1:
+                    row += 1
+                    direction = -1
+                elif row == 0:
+                    col += 1
+                    direction = -1
+                else:
+                    row -= 1
+                    col += 1
+
+            else:
+                if row == m - 1:
+                    col += 1
+                    direction = 1
+                elif col == 0:
+                    row += 1
+                    direction = 1
+                else:
+                    row += 1
+                    col -= 1
+
+        return result
+
+
+F
